7dc229c322b1e7a4a731463bad96f9f549e0ca18
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-06-13 20:12:16 +0900

    tailwind

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,16 +9,16 @@ const Register = () => {
   const navigate = useNavigate();
 
   const handleRegister = async () => {
-    if (id.length <= 4 || id.length > 10) {
-      alert("아이디가 4글자에서 10글자 이내로만 가능합니다.");
+    if (id.length < 4 || id.length <= 10) {
+      alert("아이디는 4글자 이상, 10글자 미만이어야 합니다.");
       return;
     }
-    if (password.length <= 4 || password.length > 15) {
-      alert("비밀번호가 4글자에서 15글자 이내로만 가능합니다.");
+    if (password.length < 4 || password.length <= 15) {
+      alert("비밀번호는 4글자 이상, 15글자 미만이어야 합니다.");
       return;
     }
-    if (nickname.length <= 1 || nickname.length > 10) {
-      alert("닉네임은 1글자에서 10글자 이내로만 가능합니다.");
+    if (nickname.length < 1 || nickname.length <= 10) {
+      alert("닉네임은 1글자 이상, 10글자 미만이어야 합니다.");
       return;
     }
 
